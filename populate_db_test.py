b6b86f671056076b8cfd07ec269cbfb9ed155bc8
Pavel Mac <pavel9504@gmail.com>
2026-02-11 15:20:44 +0300

    made the file more cleaner

--- populate_db_test.py
+++ populate_db_test.py
@@ -8,21 +8,22 @@ db = next(get_db())
 
 
 def main():
-    # create_tables()
-    # populate_database()
+    create_tables()
+    populate_database()
+
     query = db.query(TodayEconomicNews)
     df = pd.read_sql_query(query.statement, db.bind, params=query.statement.compile().params)
-    print(df.head())
-    current_date = df.date.dt.date.iloc[0]
-    high_impact_events_count = df[df['importance'] == 1].shape[0]
-    high_impact_events = df[df['importance'] == 1]['title'].tolist()
         
-    if df.shape[0] > 0:
-        message = \
+    if not df.empty:
+        current_date = df.date.dt.date.iloc[0]
+        high_impact_events_count = df[df['importance'] == 1].shape[0]
+        high_impact_events = df[df['importance'] == 1]['title'].tolist()
+
+        daily_summary = \
         f'Daily market summary for {current_date}: \n' \
         f'High impact events count: {high_impact_events_count} \n' \
         f'High impact events: {'\n -'.join([' '] + high_impact_events)}' 
-        print(message)
+        print(daily_summary)
 
 if __name__ == '__main__':
     main()
